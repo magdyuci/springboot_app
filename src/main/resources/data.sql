8ff34810f4d01a8a08c5ee6246e24f6f92951f18
magdalys <magdalys@strands.com>
2021-09-29 16:22:12 +0200

    user crud functions

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101,'mlopez@hh.com', 'Mario', 'Lopez', 'admin', 'ssn1', 'mlopez');
+insert into user values(102,'dalua@hh.com', 'Dalua', 'Mendez', 'admin', 'ssn12', 'dmendez');
+insert into user values(103,'amenadiel@hh.com', 'Amenadiel', 'Dios', 'admin', 'ssn123', 'adios');
